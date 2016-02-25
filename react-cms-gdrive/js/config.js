277684f72b8e51e1c90adeb6a8c8c8e0ad650ba8
CJ Wilson <cjwilson1388@gmail.com>
2016-02-25 02:12:42 -0700

    Added files via upload
    Testing gdrive-cms.

---
+++ react-cms-gdrive/js/config.js
@@ -0,0 +1,16 @@
+configReactDriveCms = {
+    author: 'React-Drive-CMS',
+    /**
+     * Add the Id for the Google Sheets Dashboard file
+     */
+    dashboardId: '1TV5xy4ENbPwNPsUFqM8ukZOkoDOzGUonCnN57EQBWDM',
+    
+    /**
+     * Add the Url Id for the Google Web App Script to forward email from the Contact Form
+     */
+    sendMailUrlId: 'https://script.google.com/macros/s/AKfycbyZBNY-Wpwrzck89VybnOtJXW-IEVHB7-dcOmL1q-Dz1vb3RcG9/exec',
+    /**
+     * Add your Disqus shortname
+     */
+    shortname: 'cjw'
+};
